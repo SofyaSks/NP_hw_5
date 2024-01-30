8b863391137a687a7558f3d2a35d927c959e53d6
sks <ssurovneva@mail.ru>
2024-01-30 18:46:33 +0500

    change

---
+++ FileServer/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HW_FileTransferServer
+{
+    internal class Program
+    {
+        private static ICollection<TcpClient> clients = new List<TcpClient>();
+
+        static async Task Main(string[] args)
+        {
+            IEnumerable<IPAddress> addresses = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress address in addresses)
+                Console.WriteLine(address);
+
+            
+
+            TcpListener listener = new TcpListener(
+                new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2024));
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                lock (clients)
+                    clients.Add(client);
+                ServeClient(client);
+            }
+        }
+
+        private static async void ServeClient(TcpClient client)
+        {
+            while (true)
+            {
+                // клиент посылает нам файл
+                //   длина имени файла
+                long fileNameLength = await ReceiveInt64(client);
+                await SendAllInt64(fileNameLength);
+                //   имя файла
+                byte[] fileNameBytes = await ReceiveFixed(client, (int)fileNameLength);
+                string fileName = Encoding.UTF8.GetString(fileNameBytes);
+                await SendAllBytes(fileNameBytes);
+
+                //   длина содержимого файла
+                long fileContentLength = await ReceiveInt64(client);
+                await SendAllInt64(fileContentLength);
+                //   содержимое файла
+                await ReceiveFileContent(client, fileName, fileContentLength);
+            }
+        }
+
+        private static async Task<byte[]> ReceiveFixed(TcpClient client, int length)
+        {
+            byte[] bytes = new byte[length];
+            int pos = 0;
+            while (pos < length)
+            {  // ReadExactlyAsync
+                int read = await client.GetStream().ReadAsync(bytes, pos, length - pos);
+                pos += read;
+            }
+            return bytes;
+        }
+        private static async Task<long> ReceiveInt64(TcpClient client)
+        {
+            byte[] bytes = await ReceiveFixed(client, sizeof(long));
+            return BitConverter.ToInt64(bytes);
+        }
+        private static async Task ReceiveFileContent(TcpClient client, string filename, long length)
+        {
+            using Stream file = File.Create(filename);
+            long pos = 0;
+            byte[] bytes = new byte[1024];
+            while (pos < length)
+            {
+                long read = await client.GetStream().ReadAsync(bytes, 0,
+                    Math.Min(bytes.Length, (int)(length - pos)));
+                // await file.WriteAsync (bytes, 0, (int) read);
+                await SendAllBytes(bytes, 0, (int)read);
+                pos += read;
+            }
+        }
+
+        private static async Task SendAllBytes(byte[] bytes)
+        {
+            foreach (TcpClient client in clients)
+                await client.GetStream().WriteAsync(bytes);
+        }
+        private static async Task SendAllBytes(byte[] bytes, int pos, int length)
+        {
+            foreach (TcpClient client in clients)
+                await client.GetStream().WriteAsync(bytes, pos, length);
+        }
+        private static async Task SendAllInt64(long value)
+        {
+            await SendAllBytes(BitConverter.GetBytes(value));
+        }
+    }
+}
+
---
+++ NP_hw_5/NP_hw_5.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+
+#define _WIN32_WINNT 0x600
+#include <WinSock2.h>   
+#include <ws2tcpip.h>
+#pragma comment (lib, "ws2_32.lib")   
+#include <Windows.h>
+#include <stdexcept>
+
+#include <fstream>
+
+using namespace std;
+
+const int PORT = 2024;
+
+int main() {
+    try {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+            throw runtime_error("WSAStartup failed");
+
+        // socket
+        SOCKET server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (server == INVALID_SOCKET)
+            throw runtime_error("socket failed");
+
+        sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+        addr.sin_port = htons(PORT);
+
+
+        if (connect(server, (const sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
+            throw runtime_error("connect failed");
+
+        ofstream file("file.txt", ios::out | ios::binary);
+
+        char buffer[1024];
+
+        int pos = 0, length = sizeof(file);
+        char content[sizeof(file)];
+
+        while (pos < length) {
+
+            fd_set check;
+            FD_ZERO(&check);
+            FD_SET(server, &check);
+
+            fd_set readable = check, writable = check, inError = check;
+            TIMEVAL wait{ 0, 0 };
+            if (select(FD_SETSIZE, &readable, &writable, &inError, &wait) == SOCKET_ERROR)
+                throw runtime_error("select failed");
+
+            if (FD_ISSET(server, &readable)) {  
+                char buffer[100];
+                int read = recv(server, buffer, sizeof(buffer), 0);
+                if (read < 0)
+                    throw runtime_error("recv failed");
+                else if (read == 0) {  
+                    cout << "Server disconnected" << endl;
+                    break;
+                }
+                cout << buffer << endl;
+            }
+
+            int read = recv(server, buffer + pos, length - pos, 0);
+  
+            for (int i = 0; i < sizeof(file); i++)
+                file << content[i];
+                                      
+            if (!file)
+                throw runtime_error("recv failed");
+
+            else if (read == 0)
+                throw runtime_error("disconnected from server");
+            pos += read;
+        }
+        cout << content;
+
+    }
+    catch (runtime_error& e) {
+        cerr << e.what() << endl;
+    }
+}
